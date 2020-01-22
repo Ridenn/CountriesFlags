169c57e5a944498c8bcf5d642d61f02dee746032
Lucas Pedrosa Ribeiro <lucaspedrosa11@hotmail.com>
2020-01-22 14:34:47 -0300

    Implementado teste unitario para a ListViewModel

---
+++ app/src/test/java/com/example/countriesflags/ListViewModelTest.kt
@@ -0,0 +1,94 @@
+package com.example.countriesflags
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.countriesflags.model.CountriesService
+import com.example.countriesflags.model.Country
+import com.example.countriesflags.viewmodel.ListViewModel
+import io.reactivex.Scheduler
+import io.reactivex.Single
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.disposables.Disposable
+import io.reactivex.internal.schedulers.ExecutorScheduler
+import io.reactivex.plugins.RxJavaPlugins
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+import java.util.concurrent.Executor
+import java.util.concurrent.TimeUnit
+
+class ListViewModelTest {
+
+    @get:Rule
+    var rule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var countriesService: CountriesService
+
+    @InjectMocks
+    var listViewModel = ListViewModel()
+
+    private var testSingle: Single<List<Country>>? = null
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Test
+    fun fetchCountriesOnSuccess() {
+
+        //TODO - cria as variáveis de teste
+        val country = Country("countryName", "capital", "url")
+        val countriesList = arrayListOf(country)
+        testSingle = Single.just(countriesList) as Single<List<Country>>?
+
+        `when`(countriesService.getCountries()).thenReturn(testSingle)
+
+        //TODO - chama o método a ser testado
+        listViewModel.refresh()
+
+        //TODO - verifica o tamanho da lista de países dentro da viewModel teste = 1
+        Assert.assertEquals(1, listViewModel.countries.value?.size)
+        Assert.assertEquals(false, listViewModel.countryLoadError.value)
+        Assert.assertEquals(false, listViewModel.loading.value)
+    }
+
+    @Test
+    fun fetchCountriesOnError(){
+
+        testSingle = Single.error(Throwable())
+
+        `when`(countriesService.getCountries()).thenReturn(testSingle)
+
+        listViewModel.refresh()
+
+        Assert.assertEquals(true, listViewModel.countryLoadError.value)
+        Assert.assertEquals(false, listViewModel.loading.value)
+    }
+
+    @Before
+    fun setupRxSchedulers() {
+        val immediate = object : Scheduler() {
+
+            override fun scheduleDirect(run: Runnable?, delay: Long, unit: TimeUnit?): Disposable {
+                return super.scheduleDirect(run, 0, unit)
+            }
+
+            override fun createWorker(): Worker {
+                return ExecutorScheduler.ExecutorWorker(Executor { it.run() })
+            }
+        }
+
+        RxJavaPlugins.setInitIoSchedulerHandler { scheduler -> immediate }
+        RxJavaPlugins.setInitComputationSchedulerHandler { scheduler -> immediate }
+        RxJavaPlugins.setInitNewThreadSchedulerHandler { scheduler -> immediate }
+        RxJavaPlugins.setInitSingleSchedulerHandler { scheduler -> immediate }
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { scheduler -> immediate }
+    }
+}
